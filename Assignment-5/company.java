d09854367e188fc95e301041f9e01c72d187dcaf
202317bh <202317bh119@wilp.bits-pilani.ac.in>
2025-09-27 23:01:08 +0530

    Create company.java

---
+++ Assignment-5/company.java
@@ -0,0 +1,21 @@
+package First_Assessment;
+
+public class company {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		employee E1= new employee("Satish", 01, 23000, "Chennai");
+		employee E2= new employee("Ravi", 02, 21000, "Bangaluru");
+		employee E3= new employee("Anita", 03, 20000, "Mumbai");
+		employee E4= new employee("Meena", 04, 13000, "Delhi");
+		employee E5= new employee("Sita", 05, 15000, "Hydrabad");
+		employee E6= new employee("Gita", 06, 27000, "Bangaluru");
+		
+		
+		
+
+ 
+	}
+
+}
